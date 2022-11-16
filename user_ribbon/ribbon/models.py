d038012b54c28e371de0321dad8ced0a505245e6
paigni <ikontai4@gmail.com>
2022-11-16 20:21:41 +0600

    Правки модели

--- user_ribbon/ribbon/models.py
+++ user_ribbon/ribbon/models.py
@@ -1,15 +1,30 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
-    title = models.CharField(max_length=100, verbose_name='Заголовок')
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics', verbose_name='Картинка')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    title = models.CharField(
+        max_length=100,
+        verbose_name='Заголовок'
+    )
+    content = models.TextField(
+        verbose_name='Текст'
+    )
+    date_posted = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+    image = models.ImageField(
+        default='default.jpg',
+        upload_to='profile_pics',
+        verbose_name='Картинка'
+    )
 
     def __str__(self):
         return self.title
