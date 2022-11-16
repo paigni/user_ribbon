534e2382d4a2a3a3b5a14deb51ac24356835eec2
paigni <ikontai4@gmail.com>
2022-11-16 20:24:00 +0600

    Удалена часть путей,добавлен путь user-posts

--- user_ribbon/user_ribbon/urls.py
+++ user_ribbon/user_ribbon/urls.py
@@ -1,26 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.conf import settings
 from ribbon.views import (
     PostListView,
     PostDetailView,
     PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
+    UserPostListView
 )
 from ribbon import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='ribbon/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='ribbon/logout.html'), name='logout'),
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('about/', views.about, name='blog-about'),
 ]
