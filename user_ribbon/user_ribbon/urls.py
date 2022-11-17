ac5eae53240b086800033f7368ac2afc8fa42d94
paigni <ikontai4@gmail.com>
2022-11-17 20:19:09 +0600

    изменён путь регистрации

--- user_ribbon/user_ribbon/urls.py
+++ user_ribbon/user_ribbon/urls.py
@@ -5,14 +5,15 @@ from ribbon.views import (
     PostListView,
     PostDetailView,
     PostCreateView,
-    UserPostListView
+    UserPostListView,
+    RegisterView
 )
-from ribbon import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-    path('register/', views.register, name='register'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='ribbon/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='ribbon/logout.html'), name='logout'),
     path('', PostListView.as_view(), name='blog-home'),
